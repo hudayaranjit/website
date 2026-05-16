99bb3820ad790f9150d009cb01c4683850836ee0
Udaya Ranjit H <hudayaranjit@gmail.com>
2026-05-16 12:02:16 +0530

    config: Allow Vite to be accessed over local network

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This enables access from your mobile phone on the same Wi-Fi
+  },
   build: {
     outDir: './backend/static',
     emptyOutDir: true,
